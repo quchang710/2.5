ae354e9dcebfdd7f9bb326b3838677c2c10e5fb6
quchang710 <trang1232024@gmail.com>
2024-10-28 23:20:35 +0700

    Initial commit

---
+++ src/Account.java
@@ -0,0 +1,16 @@
+public class Account {
+    private int id;
+    private Customer customer;
+    private double balance;
+    public Account(int id, Customer customer, double balance) {
+        this.id = id;
+        this.customer = customer;
+        this.balance = balance;
+        public int getId() {
+            return id;
+        }
+        public Customer getcustomer
+
+    }
+
+}
